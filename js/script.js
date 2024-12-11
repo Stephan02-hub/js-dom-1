96d2bf916365bfc8242c9b3007b263d8e0792967
Stephan02-hub <braghesesteven@gmail.com>
2024-12-11 01:09:03 +0100

    script

---
+++ js/script.js
@@ -0,0 +1,7 @@
+const imgLamnp = document.getElementById("lamp");
+const button = document.getElementById("btn");
+
+button.addEventListener("click", function() {
+    imgLamnp.src = 'img/yellow_lamp.png';
+    imgLamnp.alt = 'Accesa'
+});
